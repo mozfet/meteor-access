7bcfd61fe79b5abd5701f87ced3292182dc12485
Mozfet <wynand@expertbox.com>
2018-12-20 17:17:49 +0200

    fixed dependancies

--- imports/api/access/ownership.js
+++ imports/api/access/ownership.js
@@ -1,7 +1,4 @@
-/*jshint esversion: 6 */
-import Access from './access';
-import Ownership from './ownership';
-import {crud} from '../utils/crud';
+import Access from './access'
 
 /**
  * Recursively transfer a component and decendants to a user.
@@ -14,51 +11,46 @@ import {crud} from '../utils/crud';
  */
 const transferOwnership = (collectionName, documentId, userId) => {
   if (_.isUndefined(documentId || !_.isString(userId))) {
-    Log.log(['error', 'access'], 'invalid document id');
+    Log.log(['error', 'access'], 'invalid document id')
   }
 
   if (_.isUndefined(userId) || !_.isString(userId)) {
-    Log.log(['error', 'access'], 'invalid user id');
+    Log.log(['error', 'access'], 'invalid user id')
   }
 
+  // get the collection
+  const collection = Mongo.Collection.get(collectionName)
+
   // fetch document
-  const doc = crud(collectionName, 'findOne', documentId);
-  Log.log(['debug', 'access'], 'transferOwnership.document:', doc);
+  const doc = collection.findOne(documentId)
+  Log.log(['debug', 'access'], 'transferOwnership.document:', doc)
 
   // error checks
   if (_.isUndefined(doc)) {
     Log.log(['error', 'access'], 'cannot find document '+documentId+
-      ' in collection '+collectionName);
+      ' in collection '+collectionName)
   }
   if(!Access.isUser(userId)) {
-    Log.log(['error', 'access'], 'cannot find user '+userId);
+    Log.log(['error', 'access'], 'cannot find user '+userId)
   }
 
   // if user is owner of doc or is admin
   if (Access.isOwner(undefined, doc) || Access.isAdmin()) {
 
     // update component doc owner
-    crud('Components', 'update', doc._id, {$set: {ownerId: userId}});
-    // Components.update(doc._id, {$set: {ownerId: userId}});
+    collection.update(doc._id, {$set: {ownerId: userId}})
 
-    // find all children of component with same owner
-    // const children = Components.find({
-    //   ownerId: doc.ownerId,
-    //   parentId: doc._id
-    // });
-    const children = crud('Components', 'find', {
-      ownerId: doc.ownerId,
-      parentId: doc._id
-    });
-    Log.log(['debug', 'access'], 'transferOwnership.children:', children.fetch());
+    const children = collection.find({ownerId: doc.ownerId, parentId: doc._id})
+    Log.log(['debug', 'access'], 'transferOwnership.children:',
+        children.count())
 
     // for each child
     children.forEach((child) => {
-      Log.log(['debug', 'access'], 'transferOwnership.child:', child);
+      Log.log(['debug', 'access'], 'transferOwnership.child:', child)
 
       // recurse
       transferOwnership(child._id, userId);
-    });
+    })
   }
 
   //else
@@ -67,8 +59,8 @@ const transferOwnership = (collectionName, documentId, userId) => {
     //log and throw error
     Log.log(['error', 'access'], 'access denied to user '+Meteor.userId());
   }
-};
+}
 
 export default {
   transferOwnership: transferOwnership
-};
+}
--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'mozfet:access',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'Common permissions, user management, access control and document ownership.',
   git: 'https://github.com/mozfet/meteor-access.git',
   documentation: 'README.md'
@@ -12,14 +12,15 @@ Npm.depends({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.8.0.1');
-  api.use('ecmascript');
+  api.use(['ecmascript', 'underscore']);
+  api.use('dburles:mongo-collection-instances')
   api.addFiles('./imports/api/access/index.js');
 });
 
-Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('mozfet:access');
-  api.addFiles('./imports/api/access/access.test.js');
-  api.addFiles('./imports/api/access/ownership.test.js');
-});
+// Package.onTest(function(api) {
+//   api.use('ecmascript');
+//   api.use('tinytest');
+//   api.use('mozfet:access');
+//   api.addFiles('./imports/api/access/access.test.js');
+//   api.addFiles('./imports/api/access/ownership.test.js');
+// });
