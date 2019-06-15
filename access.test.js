154c2b346b2beae5f0ff5663b5c2949960c13699
Mozfet <wynand@expertbox.com>
2019-06-15 08:49:25 +0200

    test example

---
+++ access.test.js
@@ -0,0 +1,36 @@
+const linkedInUser = {
+  createdAt: new Date(),
+  _id: 'zehdiPzXhNMfNrQJZ',
+  services: {
+    linkedin: {
+      id: 'vBPGZ2E24Q',
+      accessToken: 'AQXsLkFylBbQNMgdTXP0KwmtPU4uPIUgJ1nlaNHoiyiMvwiGmGXkk3b4eBHVqEOdgZ-__TfqWJibwLFMBESys6Qq7ATz5Gw1sIETOqhhvtWSupnYYZQ2_Cv5eeN6lTGevQZz2ejBLVi-zv2wL1FE9U53fbB1qW8zaBQ8a2-5fCfHxU0u0O27F7P2P4Bp2YVxzxjGKmujbAbA0BMmaFLFMP8O-WBk-EhNzTr1gZtOUW3hxUvhecxJVrUnv2osKtmhgGYsQgSlx1dpDPJK-8KHjky9Zcobpo993qQe6t-kY6B-i6dinyG68qu7eWkIALPAJmPN8BNAdwA4KwUx5UamZs3XwFtpaA',
+      expiresAt: 1564481093180,
+      linkedinId: 'vBPGZ2E24Q',
+      firstName: [{}, {}],
+      lastName: [{}, {}],
+      profilePicture: [{}, {}],
+      emails: [[], []]],
+      emailAddress: 'wynand@expertbox.com',
+      email: 'wynand@expertbox.com'
+    }
+  },
+  profile: {
+    linkedinId: 'vBPGZ2E24Q',
+    firstName: {
+      localized: [{}, {}],
+      preferredLocale: [{}, {}]
+    },
+    lastName: {
+      localized: [{}, {}],
+      preferredLocale: [{}, {}]
+    },
+    profilePicture: {
+      displayImage: 'urn:li:digitalmediaAsset:C4D03AQEgtEtQ8vPBBQ',
+      identifiersUrl: [[], []]
+    },
+    emails: [ 'wynand@expertbox.com' ],
+    emailAddress: 'wynand@expertbox.com',
+    email: 'wynand@expertbox.com'
+  }
+}
